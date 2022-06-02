b3e53f69a0acb45f74052901507fc0d376c31bc7
Jongseo Lee <james0614ll@gmail.com>
2022-06-02 16:24:36 +0900

    add zuoye counts and bring zuoye name

--- lzs_fp.py
+++ lzs_fp.py
@@ -5,23 +5,22 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 import codecs
-#from fake_useragent import UserAgent
+from pprint import pprint
+from time import strftime
 
-options = Options()
-options.binary_location = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-#options.add_argument('headless')
-options.add_argument('window-size=1920x1080')  # 브라우저 사이즈 결정
-options.add_argument("disable-gpu")
 
 login_url = 'https://learn.tsinghua.edu.cn/f/login'
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
 path = r"C:\Users\james\Desktop\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(executable_path = path, chrome_options = options)
+driver = webdriver.Chrome(executable_path = path, options = options)
 driver.get(login_url)
 
 time.sleep(1)
 
-yourID = '2020080172' #change to 'input'
+yourID = '2020080172'
 yourPW = 'James4976!!'
 
 driver.find_element(by = By.XPATH, value = '//*[@name="i_user"]').send_keys(yourID)
@@ -47,20 +46,32 @@ time.sleep(3)
 for i in range(1, c_count + 1):
     driver.find_element(by = By.XPATH, value = f'//*[@id="suoxuecourse"]/dd[{i}]/div[2]/div[2]/ul/li[4]/a').click()
 
-time.sleep(1)
+time.sleep(3)
+
+tm = time.localtime()
+now_time = strftime('%Y-%m-%d %I:%M', tm)
 
 for i in range(1, c_count + 1):
     next_tab = driver.window_handles[i]
     driver.switch_to.window(window_name = next_tab)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    zy_table = list() # ???????잘모르겄다이
-    while (soup.select('a.btn.reviewBtn')):
-        zy_table.append(soup.select_one('#wtj tr'))
-    print(zy_table)
+    txt = driver.find_element(by = By.XPATH, value = '//*[@id="wtj_info"]').text
+    newtxt = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in txt)
+    zy_counts = [int(s) for s in newtxt.split() if s.isdigit()]
+    if bool(zy_counts):
+        zy_count = zy_counts[0]
+    else:
+        continue
+    zy_list = list()
+    for i in range(1, zy_count + 1):    
+        zy_name = driver.find_element(by = By.XPATH, value = f'//*[@id="wtj"]/tbody/tr[{i}]/td[2]').text
+        zy_ddl = driver.find_element(by = By.XPATH, value = f'//*[@id="wtj"]/tbody/tr[{i}]/td[5]').text
+        zy_timeleft = driver.find_element(by = By.XPATH, value = f'//*[@id="wtj"]/tbody/tr[{i}]/td[7]').text
+        pprint(zy_timeleft)
+        if (zy_timeleft == '已过期'):
+            break
+        zy_list.append(zy_name)
+    pprint(zy_list)
     time.sleep(2)
-
-# //*[@id="wtj"]/tbody/tr[1]/td[2]/a zuoyetimu
 
 time.sleep(3)
 
