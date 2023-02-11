12e71345eb6ba4fb145c252c959b7fa0337678b1
Jongseo Lee <james0614ll@gmail.com>
2023-02-11 15:06:08 +0000

    et complete landing page response

---
+++ TIL.md
@@ -0,0 +1,10 @@
+# 20230211 SAT
+- login 후 랜딩 페이지로 넘어가는 데 어떤 Header와 Cookie들을 넘겨줘야 하는지, 어떻게 찾고 어떤 방식으로 어디로 넘겨줘야 하는지 모르겠어서 열심히 찾아봤다. 
+- Genaral : 요청과 응답 모두에 적용
+- Request Header : 내가 요청하는데 보내야할 헤더
+- Response Header : 응답 받은 메세지의 헤더
+- 그 중 Request Header에 포함되는 항목들에 관한 설명에서 'Referer : 바로 직전에 머물었던 웹 링크 주소'라는 것을 알게 됐다. 이걸 알았으면 어디로 get/post 요청을 보내야 할지 쉽게 알았을텐데 ...
+- 진작 헤더들에 관해 공부했다면 필요한 헤더들을 보내 정상적인 응답을 받았을 것이다
+- 또 cURL command 변환기라는 좋은 도구를 발견함 'https://curl.trillworks.com/' cURL command를 집어넣으면 적절한 요청을 보내는 코드로 변환해줌 
+
+
---
+++ lzs_fp_copy.py
@@ -0,0 +1,44 @@
+import requests
+
+from bs4 import BeautifulSoup as bs
+import time
+import codecs
+from pprint import pprint
+from time import strftime
+
+LOGIN_URL = 'https://id.tsinghua.edu.cn/do/off/ui/auth/login/post/bb5df85216504820be7bba2b0ae1535b/0?/login.do'
+LANDING_URL = 'https://learn.tsinghua.edu.cn/f/wlxt/index/course/student/'
+REDIRECT_URL = 'https://learn.tsinghua.edu.cn/f/loginAccountSave?_csrf=ab951a27-9f96-44ba-875f-d1f5cc39a164'
+
+
+LOGIN_INFO = {
+    'i_user' : '**********',
+    'i_pass' : '**********'
+    }
+loginAccount = '**********'
+
+with requests.Session() as s:
+    req = s.post(LOGIN_URL, data=LOGIN_INFO)
+    time.sleep(1)
+
+    res = s.post(REDIRECT_URL, data=loginAccount)
+    
+
+    cookies = res.headers['set-cookie']
+
+    headers = {
+        'authority': 'learn.tsinghua.edu.cn',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'sec-ch-ua': '"Chromium";v="91", " Not;A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'referer': 'https://learn.tsinghua.edu.cn/f/login.do?status=SUCCESS&ticket=pm8EKA0Hpw2n01C68NGBJAH9WSB7L8Z5CYHC',
+        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+
+    response = requests.get(LANDING_URL, cookies=cookies, headers=headers)
+    #print(response.text)
